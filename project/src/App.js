816b43f9d5ff39609519cb047d14d0fbaaca2127
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2022-04-07 13:34:33 +0800

    added

--- project/src/App.js
+++ project/src/App.js
@@ -3,7 +3,7 @@ import { Table, Button, Modal } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import Tables from "./component/Table";
-import Modals from "./component/Modal";
+
 import TestModal from "./component/TestModal";
 function App() {
   const [changed, setChanged] = useState(false);
